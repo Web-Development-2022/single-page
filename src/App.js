e12b26c3b763682e32dafb032b7f0e199bb3c370
Marion <marionroussel1@hotmail.fr>
2022-09-18 17:25:00 +0300

    Single page exercise week 3.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './pages/Navbar';
+import Header from './pages/Header';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Footer from './pages/Footer';
+import NotFound from './pages/NotFound';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Header />
+      <div className = "container">
+        <Routes>
+          <Route path = "/" element = {<Home/>}/>
+          <Route path = "/about" element = {<About/>}/>
+          <Route path = "/contact" element = {<Contact/>}/>
+          <Route path ="*" element = {<NotFound/>}/>
+        </Routes>
+      </div>
+      <Footer />
+    </>
   );
 }
 
